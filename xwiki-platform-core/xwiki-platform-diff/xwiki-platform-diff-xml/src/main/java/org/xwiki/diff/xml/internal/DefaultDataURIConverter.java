0bdf1546f3a3edd8e137fb7439ceca3cf115beb9
Michael Hamann <michael.hamann@xwiki.com>
2023-04-26 17:02:48 +0200

    XWIKI-20818: Improve data URI converter
    * Add an explanation for the cache key.

--- xwiki-platform-core/xwiki-platform-diff/xwiki-platform-diff-xml/src/main/java/org/xwiki/diff/xml/internal/DefaultDataURIConverter.java
+++ xwiki-platform-core/xwiki-platform-diff/xwiki-platform-diff-xml/src/main/java/org/xwiki/diff/xml/internal/DefaultDataURIConverter.java
@@ -133,6 +133,7 @@ public class DefaultDataURIConverter implements DataURIConverter, Initializable,
     private String getCacheKey(URL url)
     {
         String userPart = this.userReferenceSerializer.serialize(CurrentUserReference.INSTANCE);
+        // Prepend the length of the user part to avoid any kind of confusion between user and URL.
         return String.format("%d:%s:%s", userPart.length(), userPart, url.toString());
     }
 
